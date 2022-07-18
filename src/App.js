a744744e6fafb8fa1fa743d183bd9cfa3eb95e87
aimeemisaki <aimeejmisaki@gmail.com>
2022-07-18 16:16:14 -0700

    getting compare to work using hardcode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Form from './Pages/Form';
 import Compare from './Pages/Compare';
 import Book from './Pages/Book';
@@ -8,37 +9,10 @@ import NavBar from './Components/NavBar'
 
 function App() {
 
-    const options = {
-      'headers': {
-        'Authorization': 'Basic YzUyYjczZDk1NTIxOThhNTM3YzA5MDA4OThlOWQ1NTE6MTkxMGQ2M2IxYmFlY2Q4NTMyOTAzNzcwN2JlMDhhZjE=',
-      }
-      // 'destionation_type': 'State',
-      // 'name': 'CA'
-    };
-    
-  
-    const getData = () => {
-      fetch('https://api.roadgoat.com/api/v2/destinations/los-angeles-ca-usa', options)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          console.log(result)
-        },
-        (error) => {
-          console.log(error)
-        }
-      )
-    }
-  
-    useEffect(() => {
-      getData()
-    }, [])
-
-
   return (
     <div>
       <NavBar />
-      <Form />
+      <Compare />
       <main>
         {/* <Routes>
           <Route path ="/" element={<Form />} />
