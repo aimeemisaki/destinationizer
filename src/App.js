f1648f26d4a975a92e94050b60f567f8f71f46ff
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-09-19 02:48:31 +0900

    creates Navigation.css and styles Navigation.js font in Navigation.css

--- src/App.js
+++ src/App.js
@@ -5,56 +5,13 @@ import React, { useState } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Search from './components/search/searchForm'
 import Compare from './components/searchResults/Compare';
-// import Book from './Pages/Book';
-import Navigation from './components/Navigation';
+import Navigation from './components/navigation/Navigation';
 
 
 
 
 function App() {
 
-  
-
-  
-  // const [city1, setCity1] = useState();
-  // const [city2, setCity2] = useState();
-  // const [search1, setSearch1] = useState('');
-  // const [search2, setSearch2] = useState('');
-  // const [searchState1, setSearchState1] = useState('State');
-  // const [searchState2, setSearchState2] = useState('State');
-  // const [error, setError] = useState('');
-  // let USA = 'usa'
-  
-  
-
-  // async function getData1(search1, searchState1) {
-  //   const data = await fetch(`https://api.roadgoat.com/api/v2/destinations/${search1}-${searchState1}-${USA}`, options)
-  //   const res = await data.json()
-  //     setCity1(res);
-  //   if(data.status === 401 || data.status === 402){
-  //     setError("Invalid request!")
-  //   } else if (data.status === 404) {
-  //     setError("Please enter a valid city or town name with the proper state.")
-  //   } else if (data.status >= 400) {
-  //     setError("Something went wrong!")
-  //   }
-  //   console.log(res.data.attributes)
-  // }
-
-  
-
-  // async function getData2(search2, searchState2) {
-  //   const data = await fetch(`https://api.roadgoat.com/api/v2/destinations/${search2}-${searchState2}-${USA}`, options)
-  //   const res = await data.json()
-  //     setCity2(res);
-  //   if(data.status === 401 || data.status === 402){
-  //     setError("Invalid request!")
-  //   } else if (data.status === 404) {
-  //     setError("Please enter a valid city or town name with the proper state.")
-  //   } else if (data.status >= 400) {
-  //     setError("Something went wrong!")
-  //   }
-  // }
   
   return (
     <>
--- src/Components/navigation/Navigation.js
+++ src/Components/navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import './Navigation.css';
 
 const Navigation = () => {
     return (
