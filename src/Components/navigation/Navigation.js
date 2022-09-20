bc2b30ccd0686fa6502d74d269cfa14f05822789
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-09-20 20:54:45 +0900

    removes Nav.text and adds background color to navbar in Navigation.css

--- src/Components/navigation/Navigation.js
+++ src/Components/navigation/Navigation.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 import './Navigation.css';
 
 const Navigation = () => {
     return (
-        <Navbar collapseOnSelect variant='light' expand='lg' fixed="top">
+        <Navbar collapseOnSelect variant='light' expand='lg' fixed="top" id="navbar">
 			<Container>
 				<Navbar.Brand as={Link} to='/'>
 					<h2 id="navbar-brand">DESTINATIONIZER</h2>
 				</Navbar.Brand>
-				<Navbar.Toggle aria-controls='basic-navbar-nav' />
-				<Navbar.Collapse id='basic-navbar-nav'>
-					<Nav>
-						<Nav.Link as={Link} to='/' id="navbar-text">
-							ABOUT
-						</Nav.Link>
-					</Nav>
-				</Navbar.Collapse>
 			</Container>
 		</Navbar>
     )
